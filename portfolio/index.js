3af8267f10d06bdccdb2100d513e65dd18e1d5a1
Abdi Kumela <150321375+Abdi036@users.noreply.github.com>
2023-12-07 00:39:52 -0800

    Add files via upload
    added opacity effect on hovering at the projects

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,16 @@
-"use strict";
+const my_projects = document.querySelector(".my_projects");
+
+my_projects.addEventListener("mouseover", function (e) {
+  hovering(e, 0.5);
+});
+
+my_projects.addEventListener("mouseout", function (e) {
+  hovering(e, 1);
+});
+function hovering(e, opp) {
+  const link = e.target.closest(".content");
+  if (link) {
+    const siblings = link.parentElement.querySelectorAll(".content");
+    siblings.forEach((el) => (el.style.opacity = el === link ? 1 : opp));
+  }
+}
